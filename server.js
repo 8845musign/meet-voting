88f205b942c7a736c8aea4f5d44899c603d80061
8845musign <8845musign@gmail.com>
2017-05-13 16:37:36 +0900

    #1 Add Connect MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const next = require('next')
+const co = require('co')
+const { MongoClient } = require('mongodb')
 
 const api = require('./api')
 
@@ -7,11 +9,19 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-.then(() => {
+// MongoDB
+const MONGO_URL = 'mongodb://localhost/meet-voting'
+
+co(function * () {
+  yield app.prepare()
+  console.log('> App prepared.')
+
+  const db = yield MongoClient.connect(MONGO_URL)
+  console.log(`> Connect to ${MONGO_URL}`)
+
   const server = express()
 
-  server.use('/api', api())
+  server.use('/api', api(db))
 
   server.get('*', (req, res) => {
     return handle(req, res)
@@ -21,4 +31,4 @@ app.prepare()
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
-})
+}).catch(error => console.error(error.stack))
