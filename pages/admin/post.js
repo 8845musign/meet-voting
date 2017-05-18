135ba1d2362d080a8980f3059c99d7b3a78d4cfd
8845musign <8845musign@gmail.com>
2017-05-19 08:44:47 +0900

    #1 Add bootsrap load

--- pages/admin/post.js
+++ pages/admin/post.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
 import withRedux from 'next-redux-wrapper'
 
 import { initStore } from '../../redux/stores/PostStore'
 import Post from '../../components/Admin/Post'
 
 const Page = () => (
-  <Post />
+  <div>
+    <Head>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link rel='stylesheet' href='/static/css/bootstrap.min.css' />
+    </Head>
+    <Post />
+  </div>
 )
 
 export default withRedux(initStore)(Page)
